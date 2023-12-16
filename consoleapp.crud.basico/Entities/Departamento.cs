5f32f4ca912e85dea95eb7f2ca1d4784c145e40f
Hugo Rodrigues <hugorod81@hotmail.com>
2023-12-15 23:58:10 -0300

    Criação das entidades

---
+++ consoleapp.crud.basico/Entities/Departamento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleapp.crud.basico.Entities
+{
+    public class Departamento : Entity
+    {
+        public string? Nome { get; set; }
+
+    }
+}
