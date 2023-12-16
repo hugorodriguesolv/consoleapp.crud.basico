bb9953da89b64d76d9f08036e02cd05854be3c61
Hugo Rodrigues <hugorod81@hotmail.com>
2023-12-15 23:21:07 -0300

    Ajuste da automação da criação da base dados

--- docker/sql-server/scripts/carga-dados.sql
+++ docker/sql-server/scripts/carga-dados.sql
@@ -1,151 +1,150 @@
-/* 
-	CRUD
-	C - CREATE
-	R - READ
-	U - UPDATE
-	D - DELETE
-*/
-
-USE GEEKJOBS
-
-GO 
-
-SET NOCOUNT ON;
-
-GO
-
--- D - DELETE
-
-DELETE Pessoa
-DELETE Departamento
-DELETE Cidade
-DELETE Estado
-
-GO
-
--- C - CREATE
-
-DBCC CHECKIDENT ('dbo.Pessoa', RESEED, 0);
-DBCC CHECKIDENT ('dbo.Departamento', RESEED, 0);
-DBCC CHECKIDENT ('dbo.Cidade', RESEED, 0);
-DBCC CHECKIDENT ('dbo.Estado', RESEED, 0);
-
-INSERT INTO Estado (Nome) VALUES ('São Paulo')
-
-DECLARE @IDESTADO AS INT
-SET @IDESTADO = @@IDENTITY
-
-INSERT INTO Cidade (Nome, Populacao, IdEstado) VALUES ('São Paulo', 10000000, @IDESTADO)
-INSERT INTO Departamento (Nome, IdCidade) VALUES ('Suprimento', @@IDENTITY)
-INSERT INTO Pessoa (Nome, IdDepartamento) VALUES ('Hugo', @@IDENTITY)
-
-INSERT INTO Cidade (Nome, Populacao, IdEstado) VALUES ('Cotia', 10000000, @IDESTADO)
-INSERT INTO Departamento (Nome, IdCidade) VALUES ('Centro de Distribuição', @@IDENTITY)
-INSERT INTO Pessoa (Nome, IdDepartamento) VALUES ('Pedro Lucas', @@IDENTITY)
+USE geekjobs
 
 GO
 
 
-INSERT INTO Estado (Nome) VALUES ('Ceará')
+IF NOT EXISTS (SELECT NAME FROM SYS.tables WHERE NAME = 'Cidade')
+BEGIN
 
-DECLARE @IDESTADO AS INT
-SET @IDESTADO = @@IDENTITY
+	/* 
+		CRUD
+		C - CREATE
+		R - READ
+		U - UPDATE
+		D - DELETE
+	*/
 
-
-INSERT INTO Cidade (Nome, Populacao, IdEstado) VALUES ('Pedra Branca', 300000, @IDESTADO)
-INSERT INTO Departamento (Nome, IdCidade) VALUES ('RH', @@IDENTITY)
-INSERT INTO Pessoa (Nome, IdDepartamento) VALUES ('Caio', @@IDENTITY)
-
-INSERT INTO Cidade (Nome, Populacao, IdEstado) VALUES ('Fortaleza', 2000000, @IDESTADO)
-INSERT INTO Departamento (Nome, IdCidade) VALUES ('Vendas', @@IDENTITY)
-INSERT INTO Pessoa (Nome, IdDepartamento) VALUES ('Sérgio', @@IDENTITY)
-
-GO
-
-/*
--- R - READ
-
---SELECT @@IDENTITY
---SELECT * FROM Estado
---SELECT * FROM Cidade
---SELECT * FROM Departamento
---SELECT * FROM Pessoa
+	USE GEEKJOBS
 
 
-SELECT 
-	PES.Id AS 'CodigoPessoa',
-	PES.Nome,
-	PES.IdDepartamento,
-	DEP.Nome,
-	DEP.IdCidade,
-	CID.Nome,
-	CID.Populacao,
-	CID.IdEstado,
-	EST.Nome
-FROM
-	Departamento AS DEP 
-	INNER JOIN Pessoa AS PES ON (PES.IdDepartamento = DEP.Id)
-	INNER JOIN Cidade AS CID ON (DEP.IdCidade = CID.Id)
-	INNER JOIN Estado AS EST ON (CID.IdEstado = EST.Id)
-WHERE
-	EST.ID = 1
-ORDER BY
-	DEP.Nome ASC,
-	EST.Nome DESC
+	SET NOCOUNT ON;
 
 
--- U - UPDATE
+	-- D - DELETE
 
-SELECT 
-	PES.Id AS 'CodigoPessoa',
-	PES.Nome,
-	PES.IdDepartamento,
-	DEP.Nome,
-	DEP.IdCidade,
-	CID.Nome,
-	CID.Populacao,
-	CID.IdEstado,
-	EST.Nome
-FROM
-	Departamento AS DEP 
-	INNER JOIN Pessoa AS PES ON (PES.IdDepartamento = DEP.Id)
-	INNER JOIN Cidade AS CID ON (DEP.IdCidade = CID.Id)
-	INNER JOIN Estado AS EST ON (CID.IdEstado = EST.Id)
-ORDER BY
-	PES.Nome
+	DELETE Pessoa
+	DELETE Departamento
+	DELETE Cidade
+	DELETE Estado
 
 
-UPDATE 
-	Pessoa 
-SET 
-	Nome = 'Hugo Rodrigues',
-	IdDepartamento = 2
-WHERE 
-	ID = 1
+	-- C - CREATE
+
+	DBCC CHECKIDENT ('dbo.Pessoa', RESEED, 0);
+	DBCC CHECKIDENT ('dbo.Departamento', RESEED, 0);
+	DBCC CHECKIDENT ('dbo.Cidade', RESEED, 0);
+	DBCC CHECKIDENT ('dbo.Estado', RESEED, 0);
+
+	INSERT INTO Estado (Nome) VALUES ('São Paulo')
+
+	DECLARE @IDESTADO AS INT
+	SET @IDESTADO = @@IDENTITY
+
+	INSERT INTO Cidade (Nome, Populacao, IdEstado) VALUES ('São Paulo', 10000000, @IDESTADO)
+	INSERT INTO Departamento (Nome, IdCidade) VALUES ('Suprimento', @@IDENTITY)
+	INSERT INTO Pessoa (Nome, IdDepartamento) VALUES ('Hugo', @@IDENTITY)
+
+	INSERT INTO Cidade (Nome, Populacao, IdEstado) VALUES ('Cotia', 10000000, @IDESTADO)
+	INSERT INTO Departamento (Nome, IdCidade) VALUES ('Centro de Distribuição', @@IDENTITY)
+	INSERT INTO Pessoa (Nome, IdDepartamento) VALUES ('Pedro Lucas', @@IDENTITY)
 
 
-SELECT 
-	PES.Id AS 'CodigoPessoa',
-	PES.Nome,
-	PES.IdDepartamento,
-	DEP.Nome,
-	DEP.IdCidade,
-	CID.Nome,
-	CID.Populacao,
-	CID.IdEstado,
-	EST.Nome
-FROM
-	Departamento AS DEP 
-	INNER JOIN Pessoa AS PES ON (PES.IdDepartamento = DEP.Id)
-	INNER JOIN Cidade AS CID ON (DEP.IdCidade = CID.Id)
-	INNER JOIN Estado AS EST ON (CID.IdEstado = EST.Id)
-ORDER BY
-	PES.Nome
+	INSERT INTO Estado (Nome) VALUES ('Ceará')
+	SET @IDESTADO = @@IDENTITY
+
+	INSERT INTO Cidade (Nome, Populacao, IdEstado) VALUES ('Pedra Branca', 300000, @IDESTADO)
+	INSERT INTO Departamento (Nome, IdCidade) VALUES ('RH', @@IDENTITY)
+	INSERT INTO Pessoa (Nome, IdDepartamento) VALUES ('Caio', @@IDENTITY)
+
+	INSERT INTO Cidade (Nome, Populacao, IdEstado) VALUES ('Fortaleza', 2000000, @IDESTADO)
+	INSERT INTO Departamento (Nome, IdCidade) VALUES ('Vendas', @@IDENTITY)
+	INSERT INTO Pessoa (Nome, IdDepartamento) VALUES ('Sérgio', @@IDENTITY)
 
 
-SELECT Id, Nome FROM Pessoa
-*/
+	/*
+	-- R - READ
 
-GO
+	--SELECT @@IDENTITY
+	--SELECT * FROM Estado
+	--SELECT * FROM Cidade
+	--SELECT * FROM Departamento
+	--SELECT * FROM Pessoa
 
-SET NOCOUNT OFF;
+
+	SELECT 
+		PES.Id AS 'CodigoPessoa',
+		PES.Nome,
+		PES.IdDepartamento,
+		DEP.Nome,
+		DEP.IdCidade,
+		CID.Nome,
+		CID.Populacao,
+		CID.IdEstado,
+		EST.Nome
+	FROM
+		Departamento AS DEP 
+		INNER JOIN Pessoa AS PES ON (PES.IdDepartamento = DEP.Id)
+		INNER JOIN Cidade AS CID ON (DEP.IdCidade = CID.Id)
+		INNER JOIN Estado AS EST ON (CID.IdEstado = EST.Id)
+	WHERE
+		EST.ID = 1
+	ORDER BY
+		DEP.Nome ASC,
+		EST.Nome DESC
+
+
+	-- U - UPDATE
+
+	SELECT 
+		PES.Id AS 'CodigoPessoa',
+		PES.Nome,
+		PES.IdDepartamento,
+		DEP.Nome,
+		DEP.IdCidade,
+		CID.Nome,
+		CID.Populacao,
+		CID.IdEstado,
+		EST.Nome
+	FROM
+		Departamento AS DEP 
+		INNER JOIN Pessoa AS PES ON (PES.IdDepartamento = DEP.Id)
+		INNER JOIN Cidade AS CID ON (DEP.IdCidade = CID.Id)
+		INNER JOIN Estado AS EST ON (CID.IdEstado = EST.Id)
+	ORDER BY
+		PES.Nome
+
+
+	UPDATE 
+		Pessoa 
+	SET 
+		Nome = 'Hugo Rodrigues',
+		IdDepartamento = 2
+	WHERE 
+		ID = 1
+
+
+	SELECT 
+		PES.Id AS 'CodigoPessoa',
+		PES.Nome,
+		PES.IdDepartamento,
+		DEP.Nome,
+		DEP.IdCidade,
+		CID.Nome,
+		CID.Populacao,
+		CID.IdEstado,
+		EST.Nome
+	FROM
+		Departamento AS DEP 
+		INNER JOIN Pessoa AS PES ON (PES.IdDepartamento = DEP.Id)
+		INNER JOIN Cidade AS CID ON (DEP.IdCidade = CID.Id)
+		INNER JOIN Estado AS EST ON (CID.IdEstado = EST.Id)
+	ORDER BY
+		PES.Nome
+
+
+	SELECT Id, Nome FROM Pessoa
+	*/
+
+
+	SET NOCOUNT OFF;
+END
--- docker/sql-server/scripts/script-criacao-database.sql
+++ docker/sql-server/scripts/script-criacao-database.sql
@@ -1,38 +1,39 @@
-create database geekjobs
+use master
 
 go
 
-use geekjobs
+if not exists(select 1 from sys.databases where name = 'geekjobs')
+begin
 
-go 
+	create database geekjobs
 
-create table dbo.Estado (
-	Id int identity primary key not null,
-	Nome varchar(100) not null
-)
+	use geekjobs
 
-go
 
-create table dbo.Cidade (
-	Id int identity primary key not null,
-	Nome varchar(100) not null,
-	Populacao int not null,
-	IdEstado int not null references dbo.Estado(id)
-)
+	create table dbo.Estado (
+		Id int identity primary key not null,
+		Nome varchar(100) not null
+	)
 
-go
 
-create table dbo.Departamento (
-	Id int identity primary key not null,
-	Nome varchar(50) not null,
-	IdCidade int not null references dbo.Cidade(id)
-)
+	create table dbo.Cidade (
+		Id int identity primary key not null,
+		Nome varchar(100) not null,
+		Populacao int not null,
+		IdEstado int not null references dbo.Estado(id)
+	)
 
-go
 
-create table dbo.Pessoa (
-	Id int identity primary key not null,
-	Nome varchar(50) not null,
-	IdDepartamento int not null references dbo.Departamento(id)
-)
+	create table dbo.Departamento (
+		Id int identity primary key not null,
+		Nome varchar(50) not null,
+		IdCidade int not null references dbo.Cidade(id)
+	)
 
+	create table dbo.Pessoa (
+		Id int identity primary key not null,
+		Nome varchar(50) not null,
+		IdDepartamento int not null references dbo.Departamento(id)
+	)
+
+end
