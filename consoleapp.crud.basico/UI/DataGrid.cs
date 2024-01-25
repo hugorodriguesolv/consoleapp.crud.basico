e1fb2f1b2d98bf31e7e6529ff9761d486c7eb228
Hugo Rodrigues <hugorod81@hotmail.com>
2024-01-24 22:37:40 -0300

    Estrutura básica do DataGrid

---
+++ consoleapp.crud.basico/UI/DataGrid.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleapp.crud.basico.UI
+{
+    public class DataGrid<T> where T : class
+    {
+        private IList<T> _dadosGrid;
+
+        public DataGrid()
+        { }
+
+        public DataGrid(IList<T> dadosGrid) => _dadosGrid = dadosGrid;
+
+        public void CarregarDados(IList<T> dadosGrid) => _dadosGrid = dadosGrid;
+
+        public void AdicionarLinha(T item)
+        {
+        }
+        public void RemoverLinha(int numeroLinha)
+        {
+        }
+
+        public void MontarCabecalho(T item)
+        {
+        }
+
+        public void Ordenar(int[] args)
+        { 
+            
+        }
+
+        public void DataBinding()
+        { 
+        
+        }
+    }
+}
